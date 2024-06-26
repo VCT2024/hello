18522fae5db6734c43650e72e7be06e4ef883ff3
VCT2024 <167922808+VCT2024@users.noreply.github.com>
2024-06-26 20:57:39 -0300

    projeto finalizado

---
+++ main.js
@@ -0,0 +1,8 @@
+let nomeUsuário = prompt("Qual o seu nome?");
+<!-- código acima acima ocultado -->
+<footer>
+     <p>Uma página desenvolvida por Alura Start</p>
+ </footer>
+ <script src="main.js"></script>
+</body>
+</html>
